f5d83223f5a14bd4fd8da3b80fe70019408cda1f
ZenFarhat <s3777800@student.rmit.edu.au>
2021-02-03 17:10:22 +1100

    attempt to fix refresh issue/GB

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import About from "./components/About/About";
 import Skills from "./components/Skills/Skills";
 import Projects from './components/Projects/Projects'
 import Contact from './components/Contact/Contact'
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as HashRouter, Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <div className='app'>
         <Nav />
         <Switch>
@@ -30,7 +30,7 @@ function App() {
           </Route>
         </Switch>
       </div>
-    </Router>
+    </HashRouter>
   );
 }
 
