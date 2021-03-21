93427295b3937225961b89adb927ec41af2f0e37
ZenFarhat <s3777800@student.rmit.edu.au>
2021-03-21 20:43:46 +1100

    removed a project from list

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -3,7 +3,6 @@ import Project from "./Project/Project";
 import "./Projects.css";
 import TeslaProject from "../../Images/tesla-project.jpg";
 import AmazonProject from "../../Images/amazon-project.png";
-import ResortProject from "../../Images/zresorts.jpg";
 import Portfoliov1 from "../../Images/portfoliov1.JPG";
 
 function Projects() {
@@ -26,12 +25,6 @@ function Projects() {
         img={Portfoliov1}
         deployment='https://zenfarhat.github.io/portfoliowebsite/'
         codeLink='https://github.com/ZenFarhat/portfoliowebsite'
-      />
-      <Project
-        title='Resort Website (WIP)'
-        img={ResortProject}
-        deployment='https://resort-ws.web.app/'
-        codeLink='https://github.com/ZenFarhat/resort-website'
       />
     </div>
   );
